0877a192038fdc88b5087e4873a8e79ce00517cb
shrprabh <shreyas.prabhakar97@gmail.com>
2023-11-22 22:37:27 -0600

    solve(sliding window):using set concept

---
+++ Sliding-Window/longestsubstring.py
@@ -0,0 +1,51 @@
+# 3. Longest Substring Without Repeating Characters
+# Solved
+# Medium
+
+
+# Given a string s, find the length of the longest 
+# substring
+#  without repeating characters.
+
+ 
+
+# Example 1:
+
+# Input: s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+# Example 2:
+
+# Input: s = "bbbbb"
+# Output: 1
+# Explanation: The answer is "b", with the length of 1.
+# Example 3:
+
+# Input: s = "pwwkew"
+# Output: 3
+# Explanation: The answer is "wke", with the length of 3.
+# Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        charSet= set()
+        l=0
+        res=0
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l+=1
+            charSet.add(s[r])
+            res=max(res,r-l+1)
+        return res
+
+
+res=Solution.lengthOfLongestSubstring(any,"abcabcb")
+print(res)
+
+res2=Solution.lengthOfLongestSubstring(any,"pwwkew")
+print(res2)
